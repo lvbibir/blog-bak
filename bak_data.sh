7929ff5164b7fe830d075d2e0d5ed08944afc164
lvbibir <15810243114@163.com>
2022-09-10 10:34:39 +0800

    backup files

--- bak_data.sh
+++ bak_data.sh
@@ -3,12 +3,10 @@
 # 获取脚本文件所在目录的绝对路径
 script_path=$(cd $(dirname ${0}); pwd) 
 
-# 删除wordpress备份的文件
+# 获取UpdraftPlus备份的wordpress数据（仅周一）
+if [ "$(date "+%u")" == "6" ];then
 /usr/bin/rm -f ${script_path}/backup/wordpress/*
-
-# 获取UpdraftPlus备份的wordpress数据
-if [ "$(date "+%u")" == "1" ];then
-/usr/bin/cp -p ${script_path}/data/wordpress/wp-content/updraft/backup_$(date "+%Y-%m-%d")* ${script_path}/bak/wordpress/
+/usr/bin/cp -p ${script_path}/data/wordpress/wp-content/updraft/backup_$(date "+%Y-%m-%d")* ${script_path}/backup/wordpress/
 fi
 
 # 备份到github仓库
