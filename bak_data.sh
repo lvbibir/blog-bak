634b4c648b65c27da95d06f04a389b7241d9e32c
lvbibir <15810243114@163.com>
2023-10-27 16:12:59 +0800

    删除 wordpress 站点的所有数据

--- bak_data.sh
+++ bak_data.sh
@@ -8,12 +8,6 @@ repeat() { while :; do $@ && return; sleep 30; done }
 # 获取脚本文件所在目录的绝对路径
 script_path=$(cd $(dirname ${0}); pwd) 
 
-# 获取UpdraftPlus备份的wordpress数据（仅周一）
-if [ "$(date "+%u")" == "1" ];then
-/usr/bin/rm -f ${script_path}/backup/wordpress/*
-/usr/bin/cp -p ${script_path}/data/wordpress/wp-content/updraft/backup_$(date "+%Y-%m-%d")* ${script_path}/backup/wordpress/
-fi
-
 # 备份到github仓库
 cd ${script_path}
 /usr/local/git/bin/git add .
