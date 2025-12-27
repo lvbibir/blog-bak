8bad47c6cf5915360c07463cbcba1c4b998d6cf1
lvbibir <15810243114@163.com>
2025-12-27 13:55:27 +0800

    backup files

--- bak_data.sh
+++ bak_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source /root/proxy
+
 echo "#############$(date "+%Y-%m-%d %H:%M:%S")#############"
 
 # 重复执行某个命令，直至成功，间隔30秒
@@ -12,4 +14,5 @@ script_path=$(cd $(dirname ${0}); pwd)
 cd ${script_path}
 /usr/bin/git add -A
 /usr/bin/git commit -m "backup files"
-repeat /usr/bin/git push origin main
+repeat /usr/bin/git push gitee main
+repeat /usr/bin/git push github main
